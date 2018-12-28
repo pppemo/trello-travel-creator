3b894fb813e7a9a460ee733ec2d2c47b4f278e73
Przemek Krupiński <przemek@heetch.com>
2018-12-28 19:25:35 +0100

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react'
 import Trello from './api/trello'
 import AddFlight from './containers/AddFlight'
+import { PageHeader, Nav, NavItem } from 'react-bootstrap'
+import { Switch, Route } from 'react-router'
+import { withRouter } from 'react-router-dom'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      navEventKey: null
+    }
+  }
   componentDidMount() {
     Trello.authorize()
+    const { pathname: navEventKey } = this.props.location
+    this.setState({ navEventKey })
   }
-  
+
+  onNavSelect = navEventKey => {
+    this.setState({ navEventKey })
+    this.props.history.push(navEventKey)
+  }
+
   render() {
     return (
       <div className="App">
-        <AddFlight />
+        <PageHeader>
+          Trello Travel Creator
+        </PageHeader>
+        <Nav bsStyle="pills" activeKey={this.state.navEventKey} onSelect={this.onNavSelect}>
+          <NavItem eventKey="/addflight">
+            Add flight segments
+          </NavItem>
+          <NavItem eventKey="/createtrip">
+            Create new trip
+          </NavItem>
+        </Nav>
+        <Switch>
+          <Route path="/addflight" component={AddFlight} />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App)
